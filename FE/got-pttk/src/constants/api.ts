1e009d815d27a6b35f0748ca9a3ec74f777e6704
Mikołaj Macioszczyk <macioszczyk@vialutions.com>
2022-05-27 08:11:41 +0200

    add api routes

---
+++ FE/got-pttk/src/constants/api.ts
@@ -0,0 +1,17 @@
+/* eslint-disable import/prefer-default-export */
+export const BACKEND_URL = 'https://localhost:44333'
+
+export const ASSETS_URL = `${BACKEND_URL}/confirmation/photo`
+
+export const ROUTES_URL = `${BACKEND_URL}/segment`
+export const PRIVATE_ROUTES_URL = `${BACKEND_URL}/trip/privateSegment`
+export const POINTS_URL = `${BACKEND_URL}/trip/segments`
+export const TRIPS_URL = `${BACKEND_URL}/trip`
+export const ADJUSTED_ROUTES_URL = `${BACKEND_URL}/trip/neighboringSegments`
+export const CONFIRMATION_URL = `${BACKEND_URL}/confirmation`
+export const CONFIRMATION_IMAGE_URL = `${CONFIRMATION_URL}/photo`
+export const CONFIRMATION_ADD_URL = `${CONFIRMATION_URL}/withPhoto`
+export const VERIFICATION_URL = `${BACKEND_URL}/weryfikacja`
+export const VERIFICATION_TRIPS_URL = `${BACKEND_URL}/weryfikacja/wycieczki`
+export const VERIFICATION_TRIP_URL = `${BACKEND_URL}/weryfikacja/wycieczka`
+export const MOUNTAIN_RANGES_URL = `${BACKEND_URL}/trip/mountainRanges/1`
