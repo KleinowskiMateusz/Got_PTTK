326b7e7d3cfcdac7383de5f2e7db432cb7b2b076
Mikołaj <macioszczyk.mikolaj@gmail.com>
2022-05-28 16:58:44 +0200

    verify token on BE

--- BE/KsiazeczkaPTTK/KsiazeczkaPTTK.API/Controllers/AuthController.cs
+++ BE/KsiazeczkaPTTK/KsiazeczkaPTTK.API/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using KsiazeczkaPTTK.API.ViewModels;
 using KsiazeczkaPTTK.Domain.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace KsiazeczkaPTTK.API.Controllers
 {
@@ -9,16 +10,38 @@ namespace KsiazeczkaPTTK.API.Controllers
     [Route("[controller]")]
     public class AuthController : ControllerBase
     {
-        private readonly IUserRepository _userRepository;
+        private string AdminToken;
+        private string AppId;
+        private string AppName;
 
-        public AuthController(IUserRepository userRepository)
+        private readonly IUserRepository _userRepository;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthController(IUserRepository userRepository, IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             _userRepository = userRepository;
+            _httpClientFactory = httpClientFactory;
+            AdminToken = configuration["FbAdminToken"];
+            AppId = configuration["FbAppId"];
+            AppName = configuration["FbAppName"];
         }
 
         [HttpPost("facebook")]
         public async Task<ActionResult> AuthorizeFacebook([FromBody] FacebookUserViewModel viewModel)
         {
+            try
+            {
+                var isValid = await VeriyToken(viewModel.AccessToken, viewModel.UserID);
+                if (!isValid)
+                {
+                    return Ok("unauthorized");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
             var user = await _userRepository.GetUserByEmail(viewModel.Email);
             if (user is null)
             {
@@ -35,6 +58,34 @@ namespace KsiazeczkaPTTK.API.Controllers
                 });
             }
             return Ok(user.UserRoleName.ToLower());
+        }
+
+        private async Task<bool> VeriyToken(string token, string userId)
+        {
+            var httpRequestMessage = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"https://graph.facebook.com/debug_token?input_token={AdminToken}&access_token={token}")
+            {};
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var contentString = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                var result = JsonSerializer.Deserialize<FacebookVerificationResponse>(contentString);
+
+                if (result?.data != null
+                    && result.data.app_id == AppId
+                    && result.data.application == AppName
+                    && result.data.is_valid
+                    && result.data.user_id == userId)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
